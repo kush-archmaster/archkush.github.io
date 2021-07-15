bdfe731d1b2a2e599d5021544b9711207840c333
kushagra upadhyay <53806391+kush-archmaster@users.noreply.github.com>
2021-07-15 23:46:04 +0530

    Update page-anime.js

--- js/page-anime.js
+++ js/page-anime.js
@@ -1,13 +1,12 @@
 //Js code for page transition
 
 const header = document.querySelector(".main");
-var about = document.querySelector(".about");
+var footer = document.querySelector(".footer");
 var tl = new TimelineMax();
 
 tl.fromTo(
   header,
-  4,
+  3,
   { height: "0vh" },
   { height: "100vh", ease: Power2.easeInOut }
 );
-
